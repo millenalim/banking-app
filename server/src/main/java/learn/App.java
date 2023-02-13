6af8e7652cccd2182fd7824b9c13fe260b3e61af
millenalim <millena.lim101@gmail.com>
2023-02-13 17:34:11 -0500

    created packages and added dependencies to pom file

---
+++ server/src/main/java/learn/App.java
@@ -0,0 +1,13 @@
+package learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ server/src/main/java/learn/entity/Recipient.java
@@ -0,0 +1,4 @@
+package learn.entity;
+
+public class Recipient {
+}
